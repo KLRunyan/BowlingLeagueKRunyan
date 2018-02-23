474eefbbe9e45f7403e3b4a4907be4fe8623ed2b
KLRunyan <36675311+KLRunyan@users.noreply.github.com>
2018-02-23 14:39:43 -0600

    Initial commit

---
+++ src/controller/TeamHelper.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import model.Team;
+
+public class TeamHelper {
+
+	EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("BowlingLeagueRunyan");
+	
+	
+	public void insertTeam(Team toAdd) {
+		// TODO Auto-generated method stub
+		
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(toAdd);
+		em.getTransaction().commit();
+		em.close();
+	}
+}
